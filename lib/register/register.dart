16cf9ae633b1dd7825e0c6802da99e28057f5982
yinchuangsum <yinchuang96@gmail.com>
2023-04-21 22:08:31 +0800

    register screen style

--- lib/register/register.dart
+++ lib/register/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:yuhuaren_app/shared/widget/password.dart';
 
 class RegisterScreen extends StatelessWidget {
@@ -7,35 +8,61 @@ class RegisterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextFormField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Full Name',
+      appBar: AppBar(
+        title: const Text('Register'),
+        leading: IconButton(
+          onPressed: () {
+            GoRouter.of(context).pop();
+          },
+          icon: const Icon(
+            Icons.arrow_back,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 41.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Full Name',
+              ),
             ),
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Phone Number',
+            TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Phone Number',
+              ),
             ),
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Email Address',
+            TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Email Address',
+              ),
             ),
-          ),
-          PasswordFormField(
-            labelText: 'Password',
-          ),
-          PasswordFormField(
-            labelText: 'Confirm Password',
-          ),
-          ElevatedButton(onPressed: () {}, child: Text('Submit'))
-        ],
+            PasswordFormField(
+              labelText: 'Password',
+              helperText:
+                  '6-12 characters included 1 number & Uppercase letter',
+            ),
+            PasswordFormField(
+              labelText: 'Confirm Password',
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Submit'),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/shared/widget/password.dart
+++ lib/shared/widget/password.dart
@@ -3,11 +3,17 @@ import 'package:flutter/material.dart';
 class PasswordFormField extends StatefulWidget {
   final Icon? prefixIcon;
   final String? labelText;
+  final BoxConstraints? constraints;
+  final FormFieldValidator<String>? validator;
+  final String? helperText;
 
   const PasswordFormField({
     super.key,
     this.prefixIcon,
     this.labelText,
+    this.constraints,
+    this.validator,
+    this.helperText,
   });
 
   @override
@@ -17,15 +23,21 @@ class PasswordFormField extends StatefulWidget {
 class _PasswordFormFieldState extends State<PasswordFormField> {
   var _hidden = true;
 
-
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       decoration: InputDecoration(
-          prefixIcon: widget.prefixIcon,
-          border: OutlineInputBorder(),
-          labelText: widget.labelText,
-          suffixIcon: IconButton(onPressed: toggleHidden, icon: Icon(suffixIcon))),
+        prefixIcon: widget.prefixIcon,
+        border: const OutlineInputBorder(),
+        labelText: widget.labelText,
+        constraints: widget.constraints,
+        suffixIcon: IconButton(
+          onPressed: toggleHidden,
+          icon: Icon(suffixIcon),
+        ),
+        helperText: widget.helperText,
+      ),
+      validator: widget.validator,
       obscureText: _hidden,
     );
   }

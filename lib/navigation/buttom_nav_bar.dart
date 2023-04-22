0e48d60672614738fadc189e8023f12e136a4077
yinchuangsum <yinchuang96@gmail.com>
2023-04-22 11:29:14 +0800

    bottom nav bar style and refactor

--- lib/navigation/buttom_nav_bar.dart
+++ lib/navigation/buttom_nav_bar.dart
@@ -1,68 +1,124 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:yuhuaren_app/shared/color.dart';
+
+final navBarItems = NavBarRouter([
+  NavBarItem(icon: Icons.view_agenda_outlined, label: 'Home', route: '/home'),
+  NavBarItem(
+    icon: Icons.calendar_today_outlined,
+    label: 'Schedule',
+    route: '/schedule',
+  ),
+  NavBarItem(
+    icon: Icons.notifications_none_outlined,
+    label: 'Notification',
+    route: '/notification',
+  ),
+  NavBarItem(
+    icon: Icons.account_circle_outlined,
+    label: 'Profile',
+    route: '/profile',
+  ),
+]);
+
+class NavBarItem {
+  final IconData icon;
+  final String label;
+  final String route;
+
+  NavBarItem({required this.icon, required this.label, required this.route});
+
+  BottomNavigationBarItem get bottomNavBarItem {
+    return BottomNavigationBarItem(
+      icon: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5.0),
+        child: Container(
+          constraints: const BoxConstraints(minWidth: 64, minHeight: 32),
+          child: Icon(
+            icon,
+            color: primary,
+          ),
+        ),
+      ),
+      activeIcon: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5.0),
+        child: Container(
+          constraints: const BoxConstraints(minWidth: 64, minHeight: 32),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16), color: primary),
+          child: Icon(
+            icon,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      label: label,
+    );
+  }
+}
+
+class NavBarRouter {
+  final List<NavBarItem> items;
+  final int defaultIdx = 0;
+  late NavBarItem defaultItem;
+  late Map<String, int> routeMap;
+  late final List<BottomNavigationBarItem> bottomNavBarItem;
+
+  NavBarRouter(this.items) {
+    defaultItem = items.first;
+    routeMap = _generateMap(items);
+    bottomNavBarItem =
+        items.map((item) => item.bottomNavBarItem).toList(growable: false);
+  }
+
+  Map<String, int> _generateMap(List<NavBarItem> items) {
+    return items
+        .asMap()
+        .map((index, item) => MapEntry<String, int>(item.route, index));
+  }
+
+  String getRouteByIndex(int idx) {
+    if (idx < items.length) {
+      return items[idx].route;
+    }
+
+    return defaultItem.route;
+  }
+
+  int getIdxByRoute(String route) {
+    return routeMap[route] ?? defaultIdx;
+  }
+}
 
 class BottomNavBar extends StatelessWidget {
   const BottomNavBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
+    final currentIndex = getCurrentIndex(context);
+
+    var children = navBarItems.bottomNavBarItem;
+
+    return Container(
+      decoration: const BoxDecoration(
+          border: Border(
+        top: BorderSide(
+          color: Color(0xff979797),
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.schedule),
-          label: 'Schedule',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.notifications),
-          label: 'Notification',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person),
-          label: 'Profile',
-        ),
-      ],
-      currentIndex: getCurrentIndex(context),
-      selectedItemColor: Colors.black,
-      onTap: (idx) {
-        route(context, idx);
-      },
-      unselectedItemColor: Colors.grey,
-      showUnselectedLabels: true,
-      selectedFontSize: 12,
-      unselectedFontSize: 12,
+      )),
+      child: BottomNavigationBar(
+        items: children,
+        currentIndex: currentIndex,
+        selectedItemColor: primary,
+        onTap: (idx) {
+          route(context, idx);
+        },
+        unselectedItemColor: primary,
+        showUnselectedLabels: true,
+        selectedFontSize: 12,
+        unselectedFontSize: 12,
+      ),
     );
-  }
-
-  String getRoute(int idx) {
-    switch (idx) {
-      case 1:
-        return '/schedule';
-      case 2:
-        return '/notification';
-      case 3:
-        return '/profile';
-      case 0:
-      default:
-        return '/home';
-    }
-  }
-
-  int getIdx(String route) {
-    switch (route) {
-      case '/schedule':
-        return 1;
-      case '/notification':
-        return 2;
-      case '/profile':
-        return 3;
-      case '/home':
-      default:
-        return 0;
-    }
   }
 
   void route(BuildContext context, int idx) {
@@ -72,4 +128,8 @@ class BottomNavBar extends StatelessWidget {
   int getCurrentIndex(BuildContext context) {
     return getIdx(GoRouter.of(context).location);
   }
+
+  String getRoute(int idx) => navBarItems.getRouteByIndex(idx);
+
+  int getIdx(String route) => navBarItems.getIdxByRoute(route);
 }

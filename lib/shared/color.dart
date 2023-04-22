18fad95aec686078ca84cffd38782c11a4df7e7d
yinchuangsum <yinchuang96@gmail.com>
2023-04-23 01:00:35 +0800

    color fix

--- lib/shared/color.dart
+++ lib/shared/color.dart
@@ -6,6 +6,6 @@ const primary = Color(0xff403F88);
 const secondary = Color(0xffEAEAF5);
 const icon = Color(0xffC4C4D9);
 const background = [
+  Color(0xffF3F6FF),
   Color(0xffFFFFFF),
-  Color(0xffF1F5FF),
 ];

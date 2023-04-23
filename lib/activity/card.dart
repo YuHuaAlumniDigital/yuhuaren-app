807cd2e7664feb95896b6d1124d10d85dd8c56c7
yinchuangsum <yinchuang96@gmail.com>
2023-04-23 18:59:47 +0800

    activity card styles

--- lib/activity/card.dart
+++ lib/activity/card.dart
@@ -1,50 +1,128 @@
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:yuhuaren_app/shared/color.dart';
 
 class ActivityCard extends StatelessWidget {
-  final Uint8List data;
+  final String title = 'Chinese New Year Ceremony';
+  final String date = '11/1/2024 Saturday, at 6.30p.m.';
+  final String tag = 'SJKC';
 
   const ActivityCard({
     super.key,
-    required this.data,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 250.0,
+      constraints: BoxConstraints(
+        maxWidth: 300,
+      ),
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12), color: Colors.grey),
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Color(0xff4DA5A4DE),
+            blurRadius: 2,
+            spreadRadius: 0,
+          ),
+          BoxShadow(
+            color: Color(0xff406664CC),
+            blurRadius: 14,
+            spreadRadius: 0,
+            offset: Offset(0, 1),
+          ),
+        ],
+      ),
       child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image.memory(data),
+          Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.asset(
+                  'assets/images/test_activity.png',
+                ),
+              ),
+              Container(
+                  child: Align(
+                      alignment: Alignment.topRight,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 12,
+                              vertical: 8,
+                            ),
+                            child: Text(
+                              tag,
+                              style: TextStyle(
+                                color: primary,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            )),
+                      ))),
+            ],
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.fromLTRB(16, 16, 5, 8),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
-                  child: Row(
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('title'),
-                          Text('subtitle'),
-                        ],
-                      ),
-                      Spacer(),
-                      ElevatedButton(onPressed: () {}, child: Text('SJKC'))
-                    ],
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
                   ),
                 ),
-                Text('description.......\n'
-                    '.......'),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                  child: Text(
+                    date,
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 9, 0, 10),
+                  child: SizedBox(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 220,
+                          child: Text(
+                            '.asdfasdfasdfasdfasdfadfadsfasdfasdfasdfasdfsdfasdfasdfasdfasdfasdfasddsaasdfsdsdfsdsdfasd',
+                            style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w300,
+                                height: 1.4),
+                          ),
+                        ),
+                        Spacer(),
+                        IconButton(
+                          padding: EdgeInsets.zero,
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.favorite,
+                              color: primary,
+                              size: 26,
+                            ))
+                      ],
+                    ),
+                  ),
+                )
               ],
             ),
           ),
@@ -55,19 +133,17 @@ class ActivityCard extends StatelessWidget {
 }
 
 class MinimizedActivityCard extends StatelessWidget {
-  final Uint8List data;
-
-  const MinimizedActivityCard({Key? key, required this.data}) : super(key: key);
+  const MinimizedActivityCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       // width: 250.0,
       // height: 120,
-      constraints: BoxConstraints(maxHeight: 120, maxWidth: 250),
+      constraints: BoxConstraints(maxHeight: 120, maxWidth: 300),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(12),
-        color: Colors.grey,
+        color: Colors.white,
       ),
       child: Row(
         children: [
@@ -100,10 +176,11 @@ class MinimizedActivityCard extends StatelessWidget {
             ),
             child: ClipRRect(
                 borderRadius: BorderRadius.circular(12),
-                child: Image.memory(
-                  data,
+                child: Image.asset(
+                  'assets/images/test_activity.png',
                   fit: BoxFit.cover,
-                )),
+                ),
+            ),
           ),
         ],
       ),
